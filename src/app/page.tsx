50bb1d4e1bf06e7e1a8d369731ec86fc6b2d88eb
Dokyun Lim <im.koremp@gmail.com>
2024-09-08 03:12:05 +0900

    feat: add basic home-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
-export const Home = () => {
-  return (
-    <div>
-      <div>
-        청소년 도우미
-      </div>
+import Link from "next/link";
 
-    </div>
+const HomePage = () => {
+  return (
+    <section className="flex flex-col items-center gap-4">
+      <h1 className="text-5xl font-bold mt-20">Korean Youth Support</h1>
+      {/* 시간 되면 슬라이더 만들기 */}
+      <div className="flex flex-col items-center gap-4">
+        <Link href="/help-center-location" className="text-2xl">
+            지역 센터 알아보기
+        </Link>
+        <Link href="/card-news" className="text-2xl">
+          카드 뉴스 보기
+        </Link>
+      </div>
+    </section>
   );
 };
+
+export default HomePage;
